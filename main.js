a67b786ff0391ec24a47abc9b11703ead7bbeebe
Sandra Sánchez <sansanvirom@gmail.com>
2023-02-05 14:08:45 -0400

    Se crearon el formulario de contacto y el footer de redes sociales.

--- main.js
+++ main.js
@@ -98,3 +98,95 @@ window.onload = function () {
     renderizarImagen();
 } 
 
+
+
+
+
+
+
+var input = document.getElementById("fname");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("femail").focus();
+  }
+});
+var input2 = document.getElementById("femail");
+input2.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("fmessage").focus();
+  }
+});
+var input3 = document.getElementById("fmessage");
+input3.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("fname").focus();
+  }
+});
+function validateForm() {
+  var sección = document.getElementById("contact");
+
+  var validar=0;
+  var c1 = document.forms["myForm"]["fname"].value;
+  var c2 = document.forms["myForm"]["femail"].value;
+  var c3 = document.forms["myForm"]["fmessage"].value;
+  
+  if (c1 == "" || c1 == null) {
+    alert("Ingrese su nombre.");
+    validar=1;
+    document.getElementById("fname").focus();
+     return false;
+
+    
+  }
+  if (c2 == "" || c2 == null) {
+    alert("Ingrese su correo electrónico.");
+    validar=1;
+    document.getElementById("femail").focus();
+    return false;
+  } 
+
+
+if (c3 == "" || c3 == null) {
+  alert("Ingrese su mensaje.");
+  validar=1;
+  document.getElementById("fmessage").focus();
+  return false;
+}
+
+
+
+      
+
+    //if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(c2)) ) {
+       // alert("Ingrese un correo electrónico válido.");
+       // return false;
+//}
+
+var regex = /^[a-zA-Z]+$/;
+      if(!regex.test(c1)){
+      	alert("Error. Los nombres no pueden tener números ni caracteres especiales.");
+        validar=1;
+        document.getElementById("fname").focus(); 
+        return false;
+      } 
+      var regex2 = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+      if(!regex2.test(c2)){
+      	alert("Error. Ingrese un correo electrónico válido.");
+        validar=1;
+        document.getElementById("femail").focus(); 
+        return false;
+      }
+    
+      if(validar==0){
+  alert("Nombre: "+c1+"\n"+"Email: "+c2+"\n"+"Mensaje: "+c3+"\n"+"¡Su mensaje ha sido enviado!");
+  document.getElementById("fname").focus();
+  return true;
+}
+
+sección.style.display = "block";
+  
+}
+
